206f3cd9b58029ac7253a0eb47280f9136919caa
Muh Hilman Sholehudin <muhhilmansholehudin@gmail.com>
2024-04-13 11:58:05 +0700

    Master - Update Tampilan Edit & Change Status User

--- resources/views/cms/admin/users/index.blade.php
+++ resources/views/cms/admin/users/index.blade.php
@@ -78,7 +78,8 @@
                                 @hasrole('super_admin')
                                 <td class="table-report__action w-56">
                                     <div class="flex justify-center items-center">
-                                        <a class="flex items-center mr-3" href="#"> <i
+                                        <a class="flex items-center mr-3" href="javascript:;" data-tw-toggle="modal"
+                                            data-tw-target="#upstat-confirmation-modal{{ $user->id }}"> <i
                                                 data-lucide="edit" class="w-4 h-4 mr-1"></i> Ubah Status </a>
                                         <a class="flex items-center text-danger" href="javascript:;" data-tw-toggle="modal"
                                             data-tw-target="#delete-confirmation-modal{{ $user->id }}"> <i data-lucide="trash-2"
@@ -88,6 +89,34 @@
                                 @endhasrole
                             </tr>
 
+                            <!-- BEGIN: Update Status Confirmation Modal -->
+                        <div id="upstat-confirmation-modal{{ $user->id }}" class="modal" tabindex="-1" aria-hidden="true">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-body p-0">
+                                        <div class="p-5 text-center">
+                                            <i data-lucide="x-circle" class="w-16 h-16 text-warning mx-auto mt-3"></i>
+                                            <div class="text-3xl mt-5">Apakah anda yakin?</div>
+                                            <div class="text-slate-500 mt-2">
+                                                Apakah anda yakin untuk mengubah status data ini?
+                                                <br>
+                                                Proses tidak akan bisa diulangi.
+                                            </div>
+                                        </div>
+                                        <div class="px-5 pb-8 text-center">
+                                            <form action="#" method="post">
+                                                @csrf
+                                                <input type="hidden" name="page" value="{{ $users->currentPage() }}">
+                                                <button type="submit" class="btn btn-warning w-24">Ubah</button>
+                                                <button type="button" data-tw-dismiss="modal"
+                                                    class="btn btn-outline-secondary w-24 ml-1">Batal</button>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- END: Update Status Confirmation Modal -->
 
                         <!-- BEGIN: Delete Confirmation Modal -->
                         <div id="delete-confirmation-modal{{ $user->id }}" class="modal" tabindex="-1" aria-hidden="true">
