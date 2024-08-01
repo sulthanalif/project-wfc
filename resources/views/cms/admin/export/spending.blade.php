bc1125e99f732966daac9a73a1e87f7c0e72bb4f
sulthanalif <sulthanalif45@gmail.com>
2024-08-01 12:19:18 +0700

    <FIX> [export] Pengeluaran

--- resources/views/cms/admin/export/spending.blade.php
+++ resources/views/cms/admin/export/spending.blade.php
@@ -1,7 +1,7 @@
 <table class="table table-report -mt-2">
     <thead>
         <tr>
-            <th class="whitespace-nowrap">Total Pemasukan</th>
+            <th class="whitespace-nowrap">Total Pengeluaran</th>
         </tr>
     </thead>
     <tbody>
@@ -18,6 +18,7 @@
         <tr>
             <th class="whitespace-nowrap">TANGGAL</th>
             <th class="whitespace-nowrap">KETERANGAN</th>
+            <th class="whitespace-nowrap">METODE</th>
             <th class="whitespace-nowrap">BIAYA TRANSFORTASI PEMBELIAN</th>
             <th class="whitespace-nowrap">BIAYA TRANSFORTASI PENJUALAN</th>
             <th class="whitespace-nowrap">BIAYA TRANSFORTASI</th>
@@ -44,126 +45,131 @@
         </tr>
     </thead>
     <tbody>
-            @foreach ($datas as $data)
-                <tr class="intro-x">
+        @foreach ($datas as $data)
+            <tr class="intro-x">
 
-                    <td>
-                        <p class="text-slate-500 flex items-center mr-3">{{ $data->date }} </p>
-                    </td>
-                    <td>
-                        <p class="text-slate-500 flex items-center mr-3">{{ $data->information }} </p>
-                    </td>
-                    <td>
-                        <p class="text-slate-500 flex items-center mr-3">
-                            {{ $data->spendingType->name == 'Biaya Transportasi Pembelian' ? $data->amount : '0' }}
-                        </p>
-                    </td>
-                    <td>
-                        <p class="text-slate-500 flex items-center mr-3">
-                            {{ $data->spendingType->name == 'Biaya Transportasi Penjualan' ? $data->amount : '0' }}
-                        </p>
-                    </td>
-                    <td>
-                        <p class="text-slate-500 flex items-center mr-3">
-                            {{ $data->spendingType->name == 'Biaya Transportasi' ? $data->amount : '0' }}
-                        </p>
-                    </td>
-                    <td>
-                        <p class="text-slate-500 flex items-center mr-3">
-                            {{ $data->spendingType->name == 'Belanja' ? $data->amount : '0' }}
-                        </p>
-                    </td>
-                    <td>
-                        <p class="text-slate-500 flex items-center mr-3">
-                            {{ $data->spendingType->name == 'Utang Dagang' ? $data->amount : '0' }}
-                        </p>
-                    </td>
-                    <td>
-                        <p class="text-slate-500 flex items-center mr-3">
-                            {{ $data->spendingType->name == 'Biaya Listrik/Telepon' ? $data->amount : '0' }}
-                        </p>
-                    </td>
-                    <td>
-                        <p class="text-slate-500 flex items-center mr-3">
-                            {{ $data->spendingType->name == 'Gaji Karyawan' ? $data->amount : '0' }}
-                        </p>
-                    </td>
-                    <td>
-                        <p class="text-slate-500 flex items-center mr-3">
-                            {{ $data->spendingType->name == 'Perlengkapan' ? $data->amount : '0' }}
-                        </p>
-                    </td>
-                    <td>
-                        <p class="text-slate-500 flex items-center mr-3">
-                            {{ $data->spendingType->name == 'Peralatan' ? $data->amount : '0' }}
-                        </p>
-                    </td>
-                    <td>
-                        <p class="text-slate-500 flex items-center mr-3">
-                            {{ $data->spendingType->name == 'Reward' ? $data->amount : '0' }}
-                        </p>
-                    </td>
-                    <td>
-                        <p class="text-slate-500 flex items-center mr-3">
-                            {{ $data->spendingType->name == 'Gift' ? $data->amount : '0' }}
-                        </p>
-                    </td>
-                    <td>
-                        <p class="text-slate-500 flex items-center mr-3">
-                            {{ $data->spendingType->name == 'Utang Bank' ? $data->amount : '0' }}
-                        </p>
-                    </td>
-                    <td>
-                        <p class="text-slate-500 flex items-center mr-3">
-                            {{ $data->spendingType->name == 'Investasi' ? $data->amount : '0' }}
-                        </p>
-                    </td>
-                    <td>
-                        <p class="text-slate-500 flex items-center mr-3">
-                            {{ $data->spendingType->name == 'Hutang' ? $data->amount : '0' }}
-                        </p>
-                    </td>
-                    <td>
-                        <p class="text-slate-500 flex items-center mr-3">
-                            {{ $data->spendingType->name == 'Pembayaran Hutang' ? $data->amount : '0' }}
-                        </p>
-                    </td>
-                    <td>
-                        <p class="text-slate-500 flex items-center mr-3">
-                            {{ $data->spendingType->name == 'Umroh' ? $data->amount : '0' }}
-                        </p>
-                    </td>
-                    <td>
-                        <p class="text-slate-500 flex items-center mr-3">
-                            {{ $data->spendingType->name == 'Permodalan' ? $data->amount : '0' }}
-                        </p>
-                    </td>
-                    <td>
-                        <p class="text-slate-500 flex items-center mr-3">
-                            {{ $data->spendingType->name == 'BRI Link/Bukalapak' ? $data->amount : '0' }}
-                        </p>
-                    </td>
-                    <td>
-                        <p class="text-slate-500 flex items-center mr-3">
-                            {{ $data->spendingType->name == 'Pegadaian' ? $data->amount : '0' }}
-                        </p>
-                    </td>
-                    <td>
-                        <p class="text-slate-500 flex items-center mr-3">
-                            {{ $data->spendingType->name == 'Kredit Amanah' ? $data->amount : '0' }}
-                        </p>
-                    </td>
-                    <td>
-                        <p class="text-slate-500 flex items-center mr-3">
-                            {{ $data->spendingType->name == 'Biaya Event' ? $data->amount : '0' }}
-                        </p>
-                    </td>
-                    <td>
-                        <p class="text-slate-500 flex items-center mr-3">
-                            {{ $data->spendingType->name == 'Biaya Lain-lain' ? $data->amount : '0' }}
-                        </p>
-                    </td>
-                </tr>
-            @endforeach
+                <td>
+                    <p class="text-slate-500 flex items-center mr-3">{{ $data->date }} </p>
+                </td>
+                <td>
+                    <p class="text-slate-500 flex items-center mr-3">{{ $data->information }} </p>
+                </td>
+                <td>
+                    <p class="text-slate-500 flex items-center mr-3">
+                        {{ $data->method == 'tunai' ? 'Tunai' : 'Transfer (' . $data->bank . ')' }}
+                    </p>
+                </td>
+                <td>
+                    <p class="text-slate-500 flex items-center mr-3">
+                        {{ $data->spendingType->name == 'Biaya Transportasi Pembelian' ? $data->amount : '0' }}
+                    </p>
+                </td>
+                <td>
+                    <p class="text-slate-500 flex items-center mr-3">
+                        {{ $data->spendingType->name == 'Biaya Transportasi Penjualan' ? $data->amount : '0' }}
+                    </p>
+                </td>
+                <td>
+                    <p class="text-slate-500 flex items-center mr-3">
+                        {{ $data->spendingType->name == 'Biaya Transportasi' ? $data->amount : '0' }}
+                    </p>
+                </td>
+                <td>
+                    <p class="text-slate-500 flex items-center mr-3">
+                        {{ $data->spendingType->name == 'Belanja' ? $data->amount : '0' }}
+                    </p>
+                </td>
+                <td>
+                    <p class="text-slate-500 flex items-center mr-3">
+                        {{ $data->spendingType->name == 'Utang Dagang' ? $data->amount : '0' }}
+                    </p>
+                </td>
+                <td>
+                    <p class="text-slate-500 flex items-center mr-3">
+                        {{ $data->spendingType->name == 'Biaya Listrik/Telepon' ? $data->amount : '0' }}
+                    </p>
+                </td>
+                <td>
+                    <p class="text-slate-500 flex items-center mr-3">
+                        {{ $data->spendingType->name == 'Gaji Karyawan' ? $data->amount : '0' }}
+                    </p>
+                </td>
+                <td>
+                    <p class="text-slate-500 flex items-center mr-3">
+                        {{ $data->spendingType->name == 'Perlengkapan' ? $data->amount : '0' }}
+                    </p>
+                </td>
+                <td>
+                    <p class="text-slate-500 flex items-center mr-3">
+                        {{ $data->spendingType->name == 'Peralatan' ? $data->amount : '0' }}
+                    </p>
+                </td>
+                <td>
+                    <p class="text-slate-500 flex items-center mr-3">
+                        {{ $data->spendingType->name == 'Reward' ? $data->amount : '0' }}
+                    </p>
+                </td>
+                <td>
+                    <p class="text-slate-500 flex items-center mr-3">
+                        {{ $data->spendingType->name == 'Gift' ? $data->amount : '0' }}
+                    </p>
+                </td>
+                <td>
+                    <p class="text-slate-500 flex items-center mr-3">
+                        {{ $data->spendingType->name == 'Utang Bank' ? $data->amount : '0' }}
+                    </p>
+                </td>
+                <td>
+                    <p class="text-slate-500 flex items-center mr-3">
+                        {{ $data->spendingType->name == 'Investasi' ? $data->amount : '0' }}
+                    </p>
+                </td>
+                <td>
+                    <p class="text-slate-500 flex items-center mr-3">
+                        {{ $data->spendingType->name == 'Hutang' ? $data->amount : '0' }}
+                    </p>
+                </td>
+                <td>
+                    <p class="text-slate-500 flex items-center mr-3">
+                        {{ $data->spendingType->name == 'Pembayaran Hutang' ? $data->amount : '0' }}
+                    </p>
+                </td>
+                <td>
+                    <p class="text-slate-500 flex items-center mr-3">
+                        {{ $data->spendingType->name == 'Umroh' ? $data->amount : '0' }}
+                    </p>
+                </td>
+                <td>
+                    <p class="text-slate-500 flex items-center mr-3">
+                        {{ $data->spendingType->name == 'Permodalan' ? $data->amount : '0' }}
+                    </p>
+                </td>
+                <td>
+                    <p class="text-slate-500 flex items-center mr-3">
+                        {{ $data->spendingType->name == 'BRI Link/Bukalapak' ? $data->amount : '0' }}
+                    </p>
+                </td>
+                <td>
+                    <p class="text-slate-500 flex items-center mr-3">
+                        {{ $data->spendingType->name == 'Pegadaian' ? $data->amount : '0' }}
+                    </p>
+                </td>
+                <td>
+                    <p class="text-slate-500 flex items-center mr-3">
+                        {{ $data->spendingType->name == 'Kredit Amanah' ? $data->amount : '0' }}
+                    </p>
+                </td>
+                <td>
+                    <p class="text-slate-500 flex items-center mr-3">
+                        {{ $data->spendingType->name == 'Biaya Event' ? $data->amount : '0' }}
+                    </p>
+                </td>
+                <td>
+                    <p class="text-slate-500 flex items-center mr-3">
+                        {{ $data->spendingType->name == 'Biaya Lain-lain' ? $data->amount : '0' }}
+                    </p>
+                </td>
+            </tr>
+        @endforeach
     </tbody>
 </table>
