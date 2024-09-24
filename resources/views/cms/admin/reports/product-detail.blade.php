c0c233f0f484b478db440e5692fca38218f1c54f
sulthanalif <sulthanalif45@gmail.com>
2024-09-24 23:30:04 +0700

    update datas

--- resources/views/cms/admin/reports/product-detail.blade.php
+++ resources/views/cms/admin/reports/product-detail.blade.php
@@ -66,12 +66,12 @@
                     </tr>
                 </thead>
                 <tbody>
-                    @if (!$paginationData)
+                    @if (!$datas)
                         <tr>
                             <td colspan="4" class="font-medium whitespace-nowrap text-center">Belum Ada Data</td>
                         </tr>
                     @else
-                        @foreach ($paginationData['data'] as $agent)
+                        @foreach ($datas as $agent)
                             <tr class="intro-x">
                                 <td>
                                     <p class="font-medium whitespace-nowrap text-center">{{ $loop->iteration }}</p>
