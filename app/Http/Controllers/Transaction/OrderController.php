7980f2b75522e2395db0af74195fc4dbaa1fadb4
sulthanalif <sulthanalif45@gmail.com>
2025-11-30 14:44:30 +0700

    fix(OrderController): check for non-rejected orders before creating new one

--- app/Http/Controllers/Transaction/OrderController.php
+++ app/Http/Controllers/Transaction/OrderController.php
@@ -159,7 +159,7 @@ class OrderController extends Controller
         $periode = Period::where('is_active', 1)->first();
 
         if ($user->roleName == 'agent') {
-            if ($user->order()?->exists() && $user->order()?->first()->created_at < $periode->end_date) return back()->with('error', 'Selesaikan dulu pesanan pada periode ini');
+            if ($user->order()?->where('status', '!=', 'reject')->exists() && $user->order()?->where('status', '!=', 'reject')->first()->created_at < $periode->end_date) return back()->with('error', 'Selesaikan dulu pesanan pada periode ini');
         }
 
         $products = json_decode($request->products, true);
