003bc4a591b77e07b1a7955a64e5e858d1558777
sulthanalif <sulthanalif45@gmail.com>
2026-02-18 09:23:56 +0700

    feat: implement queued payment notifications
    Add ShouldQueue interface to NotificationPayment mail to process sending asynchronously.
    Create jobs table migration to store queued jobs for Laravel's queue system.

--- app/Mail/NotificationPayment.php
+++ app/Mail/NotificationPayment.php
@@ -10,7 +10,7 @@ use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
-class NotificationPayment extends Mailable
+class NotificationPayment extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
